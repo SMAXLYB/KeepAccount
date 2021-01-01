db50173d0b1a3af6a3a71a402439117b18f779fb
smaxlyb <smaxlyb@qq.com>
2021-01-01 22:36:36 +0800

    确定大致布局

---
+++ app/src/main/java/life/chenshi/keepaccounts/MainActivity.kt
@@ -0,0 +1,11 @@
+package life.chenshi.keepaccounts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_keep_an_account)
+    }
+}
