788cef5e0a2af3c4b81277bbb3ee2127dd27d559
smaxlyb <smaxlyb@qq.com>
2021-02-01 22:11:26 +0800

    1.完善账本分类adapter

--- app/src/main/java/life/chenshi/keepaccounts/ui/setting/category/CategoryAdapter.kt
+++ app/src/main/java/life/chenshi/keepaccounts/ui/setting/category/CategoryAdapter.kt
@@ -1,4 +1,25 @@
 package life.chenshi.keepaccounts.ui.setting.category
 
-class CategoryAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import life.chenshi.keepaccounts.databinding.ItemCategoryBinding
+
+class CategoryAdapter(val data:List<String>) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class CategoryViewHolder(val binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
 }
--- app/src/main/java/life/chenshi/keepaccounts/ui/setting/category/SubCategoryAdapter.kt
+++ app/src/main/java/life/chenshi/keepaccounts/ui/setting/category/SubCategoryAdapter.kt
@@ -1,4 +1,25 @@
 package life.chenshi.keepaccounts.ui.setting.category
 
-class SubCategoryAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import life.chenshi.keepaccounts.databinding.ItemCategoryBinding
+
+class SubCategoryAdapter(val data: List<String>) : RecyclerView.Adapter<SubCategoryAdapter.SubCategoryViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubCategoryAdapter.SubCategoryViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: SubCategoryAdapter.SubCategoryViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class SubCategoryViewHolder(val binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
 }
