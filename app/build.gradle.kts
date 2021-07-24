a0b6d63806daee030a9e7fb528e9e9c34986ba6d
smaxlyb <smaxlyb@qq.com>
2021-07-24 19:30:21 +0800

    upgrade#ktx,datastore dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,7 +139,7 @@ dependencies {
     /**-------------------第三方库依赖--------------------**/
     // 基本库
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${Version.kotlin}")
-    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
--- app/src/main/java/life/chenshi/keepaccounts/common/utils/DataStoreUtil.kt
+++ app/src/main/java/life/chenshi/keepaccounts/common/utils/DataStoreUtil.kt
@@ -2,31 +2,73 @@ package life.chenshi.keepaccounts.common.utils
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.preferencesKey
-import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore("settings")
 
 object DataStoreUtil {
     @PublishedApi
     internal lateinit var dataStore: DataStore<Preferences>
-    private const val preferenceName = "KeepAccounts"
 
     fun init(context: Context) {
-        dataStore = context.createDataStore(name = preferenceName)
+        dataStore = context.dataStore
     }
 
     suspend inline fun <reified T : Any> writeToDataStore(key: String, value: T) {
         dataStore.edit {
-            it[preferencesKey<T>(key)] = value
+            when (value::class) {
+                Int::class -> {
+                    val pKey = intPreferencesKey(key)
+                    it[pKey] = value as Int
+                }
+                String::class -> {
+                    val pKey = stringPreferencesKey(key)
+                    it[pKey] = value as String
+                }
+                Boolean::class -> {
+                    val pKey = booleanPreferencesKey(key)
+                    it[pKey] = value as Boolean
+                }
+                Long::class -> {
+                    val pKey = longPreferencesKey(key)
+                    it[pKey] = value as Long
+                }
+                Float::class -> {
+                    val pKey = floatPreferencesKey(key)
+                    it[pKey] = value as Float
+                }
+                Double::class -> {
+                    val pKey = doublePreferencesKey(key)
+                    it[pKey] = value as Double
+                }
+            }
         }
     }
 
-    inline fun <reified T> readFromDataStore(key: String, default: T): Flow<T> {
+    inline fun <reified T : Any> readFromDataStore(key: String, default: T): Flow<T> {
         return dataStore.data.map {
-            it[preferencesKey(key)] ?: default
+            val pKey = when (default::class) {
+                Int::class -> {
+                    intPreferencesKey(key)
+                }
+                String::class -> {
+                    stringPreferencesKey(key)
+                }
+                Boolean::class -> {
+                    booleanPreferencesKey(key)
+                }
+                Long::class -> {
+                    longPreferencesKey(key)
+                }
+                Float::class -> {
+                    floatPreferencesKey(key)
+                }
+                else -> doublePreferencesKey(key)
+            }
+            (it[pKey] ?: default) as T
         }
     }
 }
