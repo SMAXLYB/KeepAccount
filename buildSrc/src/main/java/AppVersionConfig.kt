e9b668dacaaad1e2059720d24ffe4deb29cb9a95
smaxlyb <smaxlyb@qq.com>
2021-08-14 12:43:17 +0800

    fix# rename

--- buildSrc/src/main/java/AppVersionConfig.kt
+++ buildSrc/src/main/java/AppVersionConfig.kt
@@ -1,10 +1,8 @@
-object AppConfig {
+object AppVersionConfig {
     const val compileSdkVersion = 30
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
     const val versionCode = 2
     const val versionName = "alpha-0.0.1"
-
-
 }
