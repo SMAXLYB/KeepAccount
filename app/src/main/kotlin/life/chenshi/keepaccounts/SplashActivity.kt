0cb10eaa3049da3171fe15364534dc97ea6ebe64
smaxlyb <smaxlyb@qq.com>
2021-11-04 21:31:48 +0800

    [feature] 闪屏页倒计时

--- app/src/main/kotlin/life/chenshi/keepaccounts/SplashActivity.kt
+++ app/src/main/kotlin/life/chenshi/keepaccounts/SplashActivity.kt
@@ -1,50 +1,54 @@
 package life.chenshi.keepaccounts
 
+import android.annotation.SuppressLint
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import life.chenshi.keepaccounts.module.common.base.BaseActivity
 import life.chenshi.keepaccounts.ui.AppTheme
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
 
     private val mViewModel by viewModels<SplashViewModel>()
 
     override fun initView() {
-        // setContentView(R.layout.app_activity_splash)
-        // val job = flow<Int> {
-        //     for (i in 10 downTo 0) {
-        //         emit(i)
-        //         delay(1000)
-        //     }
-        // }.flowOn(Dispatchers.Default)
-        //     .onEach {
-        //         findViewById<TextView>(R.id.tv_countdown).text = "$it"
-        //     }
-        //     .launchIn(lifecycleScope)
-        //
-        // findViewById<TextView>(R.id.tv_countdown).setOnClickListener {
-        //     job.cancel()
-        // }
+        val job = flow {
+            for (i in 5 downTo 0) {
+                emit(i)
+                delay(1000)
+            }
+        }
+            .flowOn(Dispatchers.Default)
+            .onEach {
+                mViewModel.countTo(it)
+            }
+            .launchIn(lifecycleScope)
 
         setContent {
             AppTheme {
-                // SplashActivityScreen(mViewModel)
+                SplashActivityScreen(mViewModel) { job.cancel() }
             }
         }
     }
@@ -56,34 +60,26 @@ class SplashActivity : BaseActivity() {
     }
 }
 
-// @Preview
-// @Composable
-// private fun SplashActivityScreen(viewModel: SplashViewModel = viewModel()) {
-//     val counterState by viewModel.counter.collectAsState()
-//     Box() {
-//         Text("测试")
-//     }
-// }
-
-
-@Preview(showBackground = true, backgroundColor = 0xffffff)
-@Composable
-private fun SplashActivityScreen() {
-    Box() {
-        Text("测试字体")
-    }
-}
-
 @Preview
 @Composable
-private fun CircleButton(text: String = "6", onClick: () -> Unit = {}) {
+private fun SplashActivityScreen(
+    viewModel: SplashViewModel = viewModel(),
+    onClick: () -> Unit = {}
+) {
+    val counterState by viewModel.counter.collectAsState()
     Box(
-        modifier = Modifier
-            .background(Color.White, CircleShape)
-            .defaultMinSize(40.dp, 40.dp)
-            .clickable(onClick = onClick),
-        contentAlignment = Alignment.Center
+        Modifier
+            .background(color = colorResource(id = R.color.colorPrimary))
+            .fillMaxWidth()
+            .fillMaxHeight()
     ) {
-        Text(text = text)
+        Button(onClick = onClick) {
+            Text(text = counterState.toString())
+        }
+        Image(
+            painter = painterResource(id = R.drawable.common_ic_launcher_foreground),
+            contentDescription = "Logo"
+        )
+        Text("测试")
     }
 }
