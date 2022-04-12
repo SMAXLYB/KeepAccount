668939b2c0dc953c1069e948bd6024365b011ccc
smaxlyb <smaxlyb@qq.com>
2022-04-12 23:13:14 +0800

    [refactor] category/book模块拆分

---
+++ module_book/src/main/kotlin/life/chenshi/keepaccounts/module/book/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package life.chenshi.keepaccounts.module.book.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import life.chenshi.keepaccounts.module.book.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.book_activity_main)
+    }
+}
