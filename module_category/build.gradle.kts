c90ceda4fb362aa7cd5d643bb7b1f76a917dfe8b
smaxlyb <smaxlyb@gmail.com>
2022-04-15 21:09:24 +0800

    [test] 模块单独运行测试

--- module_category/build.gradle.kts
+++ module_category/build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
     id("lib-config-plugin")
     id("androidx.navigation.safeargs.kotlin")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
     buildFeatures {
         dataBinding = true
     }
+}
+
+dependencies {
+    implementation(Libs.hilt)
+    kapt(Libs.hilt_compiler)
 }
---
+++ module_category/src/main/kotlin/run_as_app/CategoryApplication.kt
@@ -0,0 +1,8 @@
+package run_as_app
+
+import dagger.hilt.android.HiltAndroidApp
+import life.chenshi.keepaccounts.module.common.base.BaseApplication
+
+@HiltAndroidApp
+class CategoryApplication  : BaseApplication() {
+}
