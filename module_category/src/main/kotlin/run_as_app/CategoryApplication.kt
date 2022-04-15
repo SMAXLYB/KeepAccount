9bc08b875899b9d6583db033d785db6faf47f123
smaxlyb <SMAXLYB@gmail.com>
2022-04-15 21:15:07 +0800

    Revert "[test] 模块单独运行测试"
    This reverts commit c90ceda4fb362aa7cd5d643bb7b1f76a917dfe8b.

--- module_category/src/main/kotlin/run_as_app/CategoryApplication.kt
+++
@@ -1,8 +0,0 @@
-package run_as_app
-
-import dagger.hilt.android.HiltAndroidApp
-import life.chenshi.keepaccounts.module.common.base.BaseApplication
-
-@HiltAndroidApp
-class CategoryApplication  : BaseApplication() {
-}
