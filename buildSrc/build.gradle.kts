81f78f02597f98743893a1ee83de2c4ae258ee1c
smaxlyb <smaxlyb@gmail.com>
2022-06-03 12:16:29 +0800

    [build] 更换阿里云仓库

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,6 +14,10 @@ gradlePlugin {
     }
 }
 repositories{
+    maven("https://maven.aliyun.com/repository/central")
+    maven("https://maven.aliyun.com/repository/public")
+    maven("https://maven.aliyun.com/repository/google")
+    maven("https://maven.aliyun.com/repository/gradle-plugin")
     maven("https://jitpack.io")
     mavenCentral()
     google()
